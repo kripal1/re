e1b8c4462d24fce19e556419e10113957ab20f5b
kripal1 <72081703+kripal1@users.noreply.github.com>
2020-11-23 17:34:53 +0530

    Add files via upload

---
+++ re/dhokla.js
@@ -0,0 +1,31 @@
+ DAL AND RICE DHOKLA :
+
+ INGREDIENTS : (1) 3 CUP RICE
+               (2) 1 CUP BLACK GRAM DAL
+               (3) 1 CUP CURD
+               (4) 6 GREEN CHILIES
+               (5) 1 TSP. GROUNDED GINGER
+               (6) SALT TO TAST 
+               (7) 1 tbsp. GHEE
+               (8) 1/2 tsp. SODA BICARBONATE
+               (9) 1/2 tbsp. BLACK PAPER
+               
+              
+               
+METHOD:
+
+Wash dal and rice together.
+ Spread on a cloth and allow to dry in the sun.
+  Grind coarsely.
+   Mix warm water and curd in a bowl. 
+   Add the grinded mixture and mix well. 
+   Cover tightly and set aside for 6 to 8 hours.
+    When fermented, add ground chilies, ginger, salt and asafetida. 
+    Mix well. 
+    Mix 1 tbsp. 
+    of ghee and soda into the batter. 
+    Pour a cupful of batter immediately onto a greased thali, 
+    sprinkle with Chilli powder and steam for 15 minutes.
+     steam the remaining batter in a similar fashion till finished.
+      When cool, cut into squares and place in a serving plate
+
